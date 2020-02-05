20f0a24d42735a186457b8828830852f268a6319
Devin Dias <devindias.tech@gmail.com>
2020-02-05 13:28:40 -0600

    set up PG data base for production

--- knexfile.js
+++ knexfile.js
@@ -17,18 +17,19 @@ module.exports = {
     }
   },
   production: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/droom.db3"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys =ON", done);
-      }
-    },
-    migrations: {
-      directory: "./data/migrations"
-    }
-  }
-};
+       client: 'pg',
+       useNullAsDefault: true,
+       connection: process.env.DATABASE_URL,
+       pool: {
+         min: 2,
+         max: 10,
+       },
+       migrations: {
+           directory: './data/migrations',
+       },
+       seeds: {
+         directory: './data/seeds',
+       },
+     },
+   }
+
